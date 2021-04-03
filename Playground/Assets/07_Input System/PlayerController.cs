a3e674762920bccfe4a395c9126a6b4f157b5fb9
9b9387 <9b9387@gmail.com>
2021-04-04 00:14:48 +0800

    add input system

---
+++ Playground/Assets/07_Input System/PlayerController.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Player ID
+    private int playerID;
+
+    [Header("Sub Behaviours")]
+    public PlayerMovement playerMovementBehaviour;
+    public PlayerAnimation playerAnimationBehaviour;
+
+
+    [Header("Input Settings")]
+    public PlayerInput playerInput;
+    public float movementSmoothingSpeed = 1f;
+    private Vector3 rawInputMovement;
+    private Vector3 smoothInputMovement;
+
+    //Action Maps
+    private string actionMapPlayerControls = "Player Controls";
+    private string actionMapMenuControls = "Menu Controls";
+
+    //Current Control Scheme
+    private string currentControlScheme;
+
+    private void Start()
+    {
+        SetupPlayer(1);
+    }
+
+    //This is called from the GameManager; when the game is being setup.
+    public void SetupPlayer(int newPlayerID)
+    {
+        playerID = newPlayerID;
+
+        currentControlScheme = playerInput.currentControlScheme;
+
+        playerMovementBehaviour.SetupBehaviour();
+        playerAnimationBehaviour.SetupBehaviour();
+    }
+
+
+    //INPUT SYSTEM ACTION METHODS --------------
+
+    //This is called from PlayerInput; when a joystick or arrow keys has been pushed.
+    //It stores the input Vector as a Vector3 to then be used by the smoothing function.
+
+
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        Vector2 inputMovement = value.ReadValue<Vector2>();
+        Debug.Log(inputMovement);
+        rawInputMovement = new Vector3(inputMovement.x, 0, inputMovement.y);
+    }
+
+    //This is called from PlayerInput, when a button has been pushed, that corresponds with the 'Attack' action
+    public void OnAttack(InputAction.CallbackContext value)
+    {
+        if (value.started)
+        {
+            playerAnimationBehaviour.PlayAttackAnimation();
+        }
+    }
+
+    //Update Loop - Used for calculating frame-based data
+    void Update()
+    {
+        CalculateMovementInputSmoothing();
+        UpdatePlayerMovement();
+        UpdatePlayerAnimationMovement();
+    }
+
+    //Input's Axes values are raw
+
+
+    void CalculateMovementInputSmoothing()
+    {
+
+        smoothInputMovement = Vector3.Lerp(smoothInputMovement, rawInputMovement, Time.deltaTime * movementSmoothingSpeed);
+
+    }
+
+    void UpdatePlayerMovement()
+    {
+        playerMovementBehaviour.UpdateMovementData(smoothInputMovement);
+    }
+
+    void UpdatePlayerAnimationMovement()
+    {
+        playerAnimationBehaviour.UpdateMovementAnimation(smoothInputMovement.magnitude);
+    }
+
+
+    public void SetInputActiveState(bool gameIsPaused)
+    {
+        switch (gameIsPaused)
+        {
+            case true:
+                playerInput.DeactivateInput();
+                break;
+
+            case false:
+                playerInput.ActivateInput();
+                break;
+        }
+    }
+
+    void RemoveAllBindingOverrides()
+    {
+        InputActionRebindingExtensions.RemoveAllBindingOverrides(playerInput.currentActionMap);
+    }
+
+
+
+    //Switching Action Maps ----
+
+
+
+    public void EnableGameplayControls()
+    {
+        playerInput.SwitchCurrentActionMap(actionMapPlayerControls);
+    }
+
+    public void EnablePauseMenuControls()
+    {
+        playerInput.SwitchCurrentActionMap(actionMapMenuControls);
+    }
+
+
+    //Get Data ----
+    public int GetPlayerID()
+    {
+        return playerID;
+    }
+
+    public InputActionAsset GetActionAsset()
+    {
+        return playerInput.actions;
+    }
+
+    public PlayerInput GetPlayerInput()
+    {
+        return playerInput;
+    }
+
+
+}
