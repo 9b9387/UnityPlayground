7c6a079f7e1b032fba58ea4b4e2227a82f60a902
9b9387 <9b9387@gmail.com>
2021-08-02 00:14:36 +0800

    add outline test

---
+++ Playground/Assets/10_URP Outline/CameraDepthCamera/CameraDepthNormalsTexture.hlsl
@@ -0,0 +1,20 @@
+﻿TEXTURE2D(_CameraDepthNormalsTexture);
+SAMPLER(sampler_CameraDepthNormalsTexture);
+
+float3 DecodeNormal(float4 enc)
+{
+    float kScale = 1.7777;
+    float3 nn = enc.xyz*float3(2*kScale,2*kScale,0) + float3(-kScale,-kScale,1);
+    float g = 2.0 / dot(nn.xyz,nn.xyz);
+    float3 n;
+    n.xy = g*nn.xy;
+    n.z = g-1;
+    return n;
+}
+
+void CameraDepthNormalsTexture_float(float2 UV, out float3 Out)
+{
+    Out = SAMPLE_TEXTURE2D(_CameraDepthNormalsTexture, sampler_CameraDepthNormalsTexture, UV);
+
+    //Out = DecodeNormal(SAMPLE_TEXTURE2D(_CameraDepthNormalsTexture, sampler_CameraDepthNormalsTexture, UV));
+}
