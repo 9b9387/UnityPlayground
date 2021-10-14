65adf0cace11c67b189a8851a7aba92f745bbf95
9b9387 <9b9387@gmail.com>
2021-10-14 23:46:38 +0800

    update render features

---
+++ Playground/Assets/12_URP Renderer Features/BlurRenderPassFeature.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class BlurRenderPassFeature : ScriptableRendererFeature
+{
+    class BlurRenderPass : ScriptableRenderPass
+    {
+        public Material material;
+        public RenderTargetIdentifier source;
+        public RenderTargetHandle tempTexture;
+
+        // This method is called before executing the render pass.
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in a performant manner.
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+            tempTexture.Init("_TempBlurTexture");
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            var cmd = CommandBufferPool.Get("BlurFeature");
+            var desc = renderingData.cameraData.cameraTargetDescriptor;
+
+            desc.depthBufferBits = 0;
+            cmd.GetTemporaryRT(tempTexture.id, desc, FilterMode.Bilinear);
+
+            Blit(cmd, source, tempTexture.Identifier(), material, 0);
+            Blit(cmd, tempTexture.Identifier(), source);
+
+            context.ExecuteCommandBuffer(cmd);
+
+            CommandBufferPool.Release(cmd);
+        }
+
+        // Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void OnCameraCleanup(CommandBuffer cmd)
+        {
+        }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(tempTexture.id);
+        }
+    }
+
+    BlurRenderPass m_ScriptablePass;
+
+    /// <inheritdoc/>
+    public override void Create()
+    {
+        m_ScriptablePass = new BlurRenderPass();
+        m_ScriptablePass.material = new Material(Shader.Find("Owlet/2D Unlit Graph/Blur Graph"));
+        m_ScriptablePass.material.SetFloat("Vector1_0cc74d0b57b742ec94b0364121a12413", 1);
+        Debug.Log("Create" + m_ScriptablePass.material);
+        // Configures where the render pass should be injected.
+        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        m_ScriptablePass.source = renderer.cameraColorTarget;
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
+
+
