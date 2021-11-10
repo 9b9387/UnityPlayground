eac1fa237339c159d350d435ad09fc03beae8489
9b9387 <9b9387@gmail.com>
2021-11-10 14:37:14 +0800

    add muti-lighting

--- Playground/Assets/11_URP Lighting Model/Common/Lighting.hlsl
+++ Playground/Assets/11_URP Lighting Model/Common/Lighting.hlsl
@@ -84,4 +84,30 @@ void LightingSpecular_float(float3 lightColor, float3 lightDir, float3 normal, f
     specular = lightColor * modifier;
 }
 
+////////////////////////////////////////
+// 额外光源计算
+////////////////////////////////////////
+void AdditionalLights_float(float3 SpecColor, float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView, out float3 Diffuse, out float3 Specular)
+{
+    float3 diffuseColor = 0;
+    float3 specularColor = 0;
+
+#ifndef SHADERGRAPH_PREVIEW
+    Smoothness = exp2(10 * Smoothness + 1);
+    WorldNormal = normalize(WorldNormal);
+    WorldView = SafeNormalize(WorldView);
+    int pixelLightCount = GetAdditionalLightsCount();
+    for (int i = 0; i < pixelLightCount; ++i)
+    {
+        Light light = GetAdditionalLight(i, WorldPosition);
+        half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+        diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
+        specularColor += LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, float4(SpecColor, 0), Smoothness);
+    }
+#endif
+
+    Diffuse = diffuseColor;
+    Specular = specularColor;
+}
+
 #endif
